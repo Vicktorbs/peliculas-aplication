52a6fad430e8624e29df9d1b3f7d83a07281e7be
Victor Bolaños Solís <viktorb.132@gmail.com>
2020-10-12 19:53:43 -0500

    Archivos y prueva de servicios

---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarteleraResponse } from '../interfaces/cartelera-response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PeliculasService {
+
+  constructor(private http: HttpClient) { }
+
+  getCartelera(): Observable<CarteleraResponse> {
+    return this.http.get<CarteleraResponse>('https://api.themoviedb.org/3/movie/now_playing?api_key=cd7741dfe31d6c74acf629d2aaba7fdd&language=es-Es&page=1')
+  }
+
+}
