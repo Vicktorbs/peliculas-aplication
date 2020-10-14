64033ba8def0e90710e556880337215f93c26bd1
Victor Bolaños Solís <viktorb.132@gmail.com>
2020-10-14 16:14:07 -0500

    Swipere integracion

--- src/app/components/slideshow/slideshow.component.ts
+++ src/app/components/slideshow/slideshow.component.ts
@@ -1,20 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/app/interfaces/cartelera-response';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-slideshow',
   templateUrl: './slideshow.component.html',
   styleUrls: ['./slideshow.component.css']
 })
-export class SlideshowComponent implements OnInit {
+export class SlideshowComponent implements OnInit, AfterViewInit {
 
   @Input() movies: Movie[];
+  public mySwiper: Swiper;
 
   constructor() { }
 
   ngOnInit() {
-    console.log(this.movies);
+    // console.log(this.movies);
     
+  }
+
+  ngAfterViewInit(): void {
+    this.mySwiper = new Swiper('.swiper-container', {
+      // Optional parameters
+      loop: true
+    });
+  }
+
+  onSlideNext(){
+    this.mySwiper.slideNext();
+  }
+
+  onSlidePrev(){
+    this.mySwiper.slidePrev();
   }
 
 }
